7b84c27b5608eac20f6b281e78f560c39604632c
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2020-05-10 14:53:26 +0300

    split log in activity into log in and sign in activity's.

--- app/src/main/java/com/example/socialdeliverysystem/LoginActivity.java
+++ app/src/main/java/com/example/socialdeliverysystem/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.socialdeliverysystem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
@@ -14,15 +15,10 @@ import java.util.regex.Pattern;
 public class LoginActivity extends AppCompatActivity {
     private TextInputLayout editTextEmailLogin;
     private TextInputLayout editTextPasswordLogin;
-    private TextInputLayout editTextFirstNameSignin;
-    private TextInputLayout editTextLastNameSignin;
-    private TextInputLayout editTextEmailSignin;
-    private TextInputLayout editTextPhoneSignin;
-    private TextInputLayout editTextIdSignin;
-    private TextInputLayout editTextAddressSignin;
-    private TextInputLayout editTextPasswordSignin;
+    private TextInputLayout getEditTextSmsLogin;
     private LinearLayoutCompat loginLayout;
-    private LinearLayoutCompat signinLayout;
+    private LinearLayoutCompat sms_login;
+    private LinearLayoutCompat password_login;
 
 
     @Override
@@ -30,136 +26,48 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         findView();
-        loginLayout.setVisibility(View.GONE);
-        signinLayout.setVisibility(View.GONE);
+        sms_login.setVisibility(View.GONE);
     }
 
-    void findView() {
+    private void findView() {
         editTextEmailLogin = findViewById(R.id.login_input_email);
         editTextPasswordLogin = findViewById(R.id.login_input_password);
-        editTextFirstNameSignin = findViewById(R.id.signin_input_firstname);
-        editTextLastNameSignin = findViewById(R.id.signin_input_lastname);
-        editTextEmailSignin = findViewById(R.id.signin_input_email);
-        editTextPhoneSignin = findViewById(R.id.signin_input_phone);
-        editTextIdSignin = findViewById(R.id.signin_input_id);
-        editTextAddressSignin = findViewById(R.id.signin_input_address);
-        editTextPasswordSignin = findViewById(R.id.signin_input_password);
+        getEditTextSmsLogin = findViewById(R.id.login_input_sms);
         loginLayout = findViewById(R.id.login_layout);
-        signinLayout = findViewById(R.id.signin_layout);
+        sms_login = findViewById(R.id.sms_entry);
+        password_login = findViewById(R.id.password_entry);
     }
 
-    private boolean validateSigninFirstName() {
-        String firstNameInput = editTextFirstNameSignin.getEditText().getText().toString().trim();
-        if (firstNameInput.isEmpty()) {
-            editTextFirstNameSignin.setError("First Name is required");
-            return false;
-        }
-        editTextFirstNameSignin.setError(null);
-        return true;
-    }
-
-    private boolean validateSigninLastName() {
-        String lastNameInput = editTextLastNameSignin.getEditText().getText().toString().trim();
-        if (lastNameInput.isEmpty()) {
-            editTextLastNameSignin.setError("Last Name is required");
-            return false;
-        }
-        editTextLastNameSignin.setError(null);
-        return true;
-    }
-
-    private boolean validateSigninPhone() {
-        String phoneInput = editTextPhoneSignin.getEditText().getText().toString().trim();
-        // Pattern phonePattern = Pattern.compile("(00972|0|\\+972)[5][0-9]{8} | (00970|0|\\+970)[5][0-9]{8} | (05[0-9]|0[12346789])([0-9]{7}) | (00972|0|\\+972|0|)[2][0-9]{7}");
-        if (!phoneInput.isEmpty() /*&&phonePattern.matcher(phoneInput).matches()*/) {
-            editTextPhoneSignin.setError(null);
-            return true;
-        }
-        String error;
-        if (phoneInput.isEmpty())
-            error = "Phone is required";
-        else
-            error = "Invalid Phone Number!";
-        editTextPhoneSignin.setError(error);
-        return false;
-    }
-
-    private boolean validateSigninID() {
-        String idInput = editTextIdSignin.getEditText().getText().toString().trim();
-        if (idInput.length() == 9) {
-            editTextIdSignin.setError(null);
-            return true;
-        }
-        String error;
-        if (idInput.isEmpty())
-            error = "ID is required";
-        else
-            error = "Invalid ID Number!";
-        editTextIdSignin.setError(error);
-        return false;
-    }
-
-    private boolean validateSigninAddress() {
-        String addressInput = editTextAddressSignin.getEditText().getText().toString().trim();
-        if (!addressInput.isEmpty()) {
-            editTextAddressSignin.setError(null);
-            return true;
-        }
-        String error;
-        if (addressInput.isEmpty())
-            error = "Address is required";
-        else
-            error = "Invalid Address!";
-        editTextAddressSignin.setError(error);
-        return false;
-    }
-
-    private boolean validateSigninEmail() {
-        String emailInput = editTextEmailSignin.getEditText().getText().toString().trim();
-        if (!emailInput.isEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            editTextEmailSignin.setError(null);
-            return true;
-        }
-        String error;
-        if (emailInput.isEmpty())
-            error = "Email is required";
-        else
-            error = "Invalid Email Address!";
-        editTextEmailSignin.setError(error);
-        return false;
-    }
-
-
-    private boolean validateSigninPassword() {
-        String passwordInput = editTextPasswordSignin.getEditText().getText().toString().trim();
-        if (Pattern.compile("((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!^&*+=-_]).{4,20})").matcher(passwordInput).matches()) {
-            editTextPasswordSignin.setError(null);
-            return true;
-        }
-        String error;
-        if (passwordInput.isEmpty())
-            error = "Password is required";
-        else
-            error = "Password must contain mix of upper and lower case letters as well as digits and one special character(4-20)";
-        editTextPasswordSignin.setError(error);
-        return false;
-    }
 
     public void logInOnClick(View view) {
-        if(loginLayout.getVisibility() == View.GONE){
-            signinLayout.setVisibility(View.GONE);
-            loginLayout.setVisibility(View.VISIBLE);
-        }
         return;
     }
 
-    public void SignInOnClick(View view) {
-        if(signinLayout.getVisibility() == View.GONE){
-            loginLayout.setVisibility(View.GONE);
-            signinLayout.setVisibility(View.VISIBLE);
+    public void logInSmsOnClick(View view) {
+        if (!checkEmailEditTextBox(editTextEmailLogin.getEditText().getText().toString())){
+            editTextEmailLogin.setError("EMAIL ADDRESS NOT CORRECT!");
             return;
         }
-        if (!validateSigninAddress() | !validateSigninEmail() | !validateSigninFirstName() | !validateSigninID() | !validateSigninLastName() | !validateSigninPassword() | !validateSigninPhone())
-            return;
+        editTextEmailLogin.setEnabled(false);
+        editTextPasswordLogin.getEditText().getText().clear();
+        password_login.setVisibility(View.GONE);
+        sms_login.setVisibility(View.VISIBLE);
+    }
+
+    private boolean checkEmailEditTextBox(String toString) {
+        //To Do!!
+        return  true;
+    }
+
+    public void logInPasswordOnClick(View view) {
+        editTextEmailLogin.setEnabled(true);
+        getEditTextSmsLogin.getEditText().getText().clear();
+        sms_login.setVisibility(View.GONE);
+        password_login.setVisibility(View.VISIBLE);
+    }
+
+    public void SignInOnClick(View view) {
+        Intent signIn = new Intent(getApplicationContext(), SigninActivity.class);
+        startActivity(signIn);
     }
 }
---
+++ app/src/main/java/com/example/socialdeliverysystem/SigninActivity.java
@@ -0,0 +1,148 @@
+package com.example.socialdeliverysystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Pattern;
+
+public class SigninActivity extends AppCompatActivity {
+
+    private TextInputLayout editTextFirstNameSignin;
+    private TextInputLayout editTextLastNameSignin;
+    private TextInputLayout editTextEmailSignin;
+    private TextInputLayout editTextPhoneSignin;
+    private TextInputLayout editTextIdSignin;
+    private TextInputLayout editTextAddressSignin;
+    private TextInputLayout editTextPasswordSignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        findView();
+    }
+
+    private void findView() {
+        editTextFirstNameSignin = findViewById(R.id.signin_input_firstname);
+        editTextLastNameSignin = findViewById(R.id.signin_input_lastname);
+        editTextEmailSignin = findViewById(R.id.signin_input_email);
+        editTextPhoneSignin = findViewById(R.id.signin_input_phone);
+        editTextIdSignin = findViewById(R.id.signin_input_id);
+        editTextAddressSignin = findViewById(R.id.signin_input_address);
+        editTextPasswordSignin = findViewById(R.id.signin_input_password);
+    }
+
+
+    private boolean validateSigninFirstName() {
+        String firstNameInput = editTextFirstNameSignin.getEditText().getText().toString().trim();
+        if (firstNameInput.isEmpty()) {
+            editTextFirstNameSignin.setError("First Name is required");
+            return false;
+        }
+        editTextFirstNameSignin.setError(null);
+        return true;
+    }
+
+    private boolean validateSigninLastName() {
+        String lastNameInput = editTextLastNameSignin.getEditText().getText().toString().trim();
+        if (lastNameInput.isEmpty()) {
+            editTextLastNameSignin.setError("Last Name is required");
+            return false;
+        }
+        editTextLastNameSignin.setError(null);
+        return true;
+    }
+
+    private boolean validateSigninPhone() {
+        String phoneInput = editTextPhoneSignin.getEditText().getText().toString().trim();
+        Pattern phonePattern = Pattern.compile("0([23489]|5[0123458]|77)([0-9]{7})");
+        if (!phoneInput.isEmpty() && phonePattern.matcher(phoneInput).matches()) {
+            editTextPhoneSignin.setError(null);
+            return true;
+        }
+        String error;
+        if (phoneInput.isEmpty())
+            error = "Phone is required";
+        else
+            error = "Invalid Phone Number!";
+        editTextPhoneSignin.setError(error);
+        return false;
+    }
+
+    private boolean validateSigninID() {
+        String idInput = editTextIdSignin.getEditText().getText().toString().trim();
+        if (idInput.length() == 9) {
+            editTextIdSignin.setError(null);
+            return true;
+        }
+        String error;
+        if (idInput.isEmpty())
+            error = "ID is required";
+        else
+            error = "Invalid ID Number!";
+        editTextIdSignin.setError(error);
+        return false;
+    }
+
+    private boolean validateSigninAddress() {
+        String addressInput = editTextAddressSignin.getEditText().getText().toString().trim();
+        if (!addressInput.isEmpty()) {
+            editTextAddressSignin.setError(null);
+            return true;
+        }
+        String error;
+        if (addressInput.isEmpty())
+            error = "Address is required";
+        else
+            error = "Invalid Address!";
+        editTextAddressSignin.setError(error);
+        return false;
+    }
+
+    private boolean validateSigninEmail() {
+        String emailInput = editTextEmailSignin.getEditText().getText().toString().trim();
+        if (!emailInput.isEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            editTextEmailSignin.setError(null);
+            return true;
+        }
+        String error;
+        if (emailInput.isEmpty())
+            error = "Email is required";
+        else
+            error = "Invalid Email Address!";
+        editTextEmailSignin.setError(error);
+        return false;
+    }
+
+
+    private boolean validateSigninPassword() {
+        String passwordInput = editTextPasswordSignin.getEditText().getText().toString().trim();
+        if (Pattern.compile("((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!^&*+=-_]).{4,20})").matcher(passwordInput).matches()) {
+            editTextPasswordSignin.setError(null);
+            return true;
+        }
+        String error;
+        if (passwordInput.isEmpty())
+            error = "Password is required";
+        else
+            error = "Password must contain mix of upper and lower case letters as well as digits and one special character(4-20)";
+        editTextPasswordSignin.setError(error);
+        return false;
+    }
+
+    public void SignInOnClick(View view) {
+        //add to firebase
+        if (!validateSigninAddress() | !validateSigninEmail() | !validateSigninFirstName() | !validateSigninID() | !validateSigninLastName() | !validateSigninPassword() | !validateSigninPhone())
+            return;
+    }
+
+    public void logInOnClick(View view) {
+        Intent logInActivity = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(logInActivity);
+    }
+}
