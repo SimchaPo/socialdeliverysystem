cf84a21a37a85ec457b89502766e7c40b1f4478d
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2020-05-10 23:19:55 +0300

    split log in activity into log in and sign in activity's.

--- app/src/main/java/com/example/socialdeliverysystem/LoginActivity.java
+++ app/src/main/java/com/example/socialdeliverysystem/LoginActivity.java
@@ -3,11 +3,11 @@ package com.example.socialdeliverysystem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
-
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.regex.Pattern;
@@ -70,4 +70,6 @@ public class LoginActivity extends AppCompatActivity {
         Intent signIn = new Intent(getApplicationContext(), SigninActivity.class);
         startActivity(signIn);
     }
+
+
 }
